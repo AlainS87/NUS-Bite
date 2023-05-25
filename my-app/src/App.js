7564af77ef4756ee3ab511334ca231a8bfeb52e2
AlainS87 <50228466+AlainS87@users.noreply.github.com>
2023-05-25 05:06:45 +0000

    first attempt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logoNUSBITE from "./logoNUSBITE.gif";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>NUS Bite</h1>
+          <img src={logoNUSBITE} className="App-logo" />
+          <h3 className="login-header">login as</h3>
+          <table className="id-table">
+            <tr>
+              <button
+                className="App-login-button-taster"
+                href="https://reactjs.org"
+              >
+                <b>NUS Tasters</b>
+              </button>
+
+              <div className="blank-between-button"></div>
+
+              <button
+                className="App-login-button-hawker"
+                href="https://reactjs.org"
+              >
+                <b>NUS Hawkers</b>
+              </button>
+            </tr>
+          </table>
+          <h2 className="or">—————OR—————</h2>
+          <button
+            className="App-login-button-visitor"
+            href="https://reactjs.org"
+          >
+            <b>NUS Visitor</b>
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
