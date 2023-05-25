4752d872dbb8a5c8190dfa6f78f14930e641491e
林玢 <hayashi@hayashis-MacBook-Pro-2.local>
2023-05-25 16:13:29 +0800

    added login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,13 @@
 import React, { Component } from "react";
-import logoNUSBITE from "./logoNUSBITE.gif";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { HomeScreen } from "./component/HomeScreen";
+import { Login } from "./component/loginScreen";
 
-class App extends React.Component {
-  render() {
+export default function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>NUS Bite</h1>
-          <img src={logoNUSBITE} className="App-logo" />
-          <h3 className="login-header">login as</h3>
-          <table className="id-table">
-            <tr>
-              <button
-                className="App-login-button-taster"
-                href="https://reactjs.org"
-              >
-                <b>NUS Tasters</b>
-              </button>
-
-              <div className="blank-between-button"></div>
-
-              <button
-                className="App-login-button-hawker"
-                href="https://reactjs.org"
-              >
-                <b>NUS Hawkers</b>
-              </button>
-            </tr>
-          </table>
-          <h2 className="or">—————OR—————</h2>
-          <button
-            className="App-login-button-visitor"
-            href="https://reactjs.org"
-          >
-            <b>NUS Visitor</b>
-          </button>
-        </header>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomeScreen />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Routes>
     );
-  }
 }
-export default App;
---
+++ my-app/src/component/HomeScreen.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import logoNUSBITE from "/Users/hayashi/Desktop/Orbital2023/NUS-Bite-Orbital2023/my-app/src/logoNUSBITE.gif";
+import "/Users/hayashi/Desktop/Orbital2023/NUS-Bite-Orbital2023/my-app/src/App.js";
+import { useNavigate } from "react-router-dom";
+import { Login } from "./loginScreen";
+
+export const HomeScreen = () => {
+  const navigate = useNavigate()
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>NUS Bite</h1>
+        <img src={logoNUSBITE} className="App-logo" />
+        <h3 className="login-header">login as</h3>
+        <table className="id-table">
+          <tr>
+            <button
+              onClick={() => navigate("Login")}
+              className="App-login-button-taster"
+              href="https://reactjs.org"
+            >
+              <b>NUS Tasters</b>
+            </button>
+
+            <div className="blank-between-button"></div>
+
+            <button
+              className="App-login-button-hawker"
+              href="https://reactjs.org"
+            >
+              <b>NUS Hawkers</b>
+            </button>
+          </tr>
+        </table>
+        <h2 className="or">—————OR—————</h2>
+        <button
+          className="App-login-button-visitor"
+          href="https://reactjs.org"
+        >
+          <b>NUS Visitor</b>
+        </button>
+      </header>
+    </div>
+  )
+}
