ca33f9348d7ab4a2e7762226451e9bfdce2f47c8
AlainS87 <50228466+AlainS87@users.noreply.github.com>
2023-06-13 07:27:16 +0000

    css added

--- my-app/src/component/hawkerMainScreen.js
+++ my-app/src/component/hawkerMainScreen.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import ResponsiveAppBar from '../Appbar'
-import { Navbar, Container, Nav, Form, Row, Col, Button } from 'react-bootstrap'
-import { useState, useEffect, useCallback } from 'react'
-import StallInfo from './stallInfo'
-import { supabase } from './supabaseData'
+import React from "react";
+import ResponsiveAppBar from "../Appbar";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
+import { useState, useEffect, useCallback } from "react";
+import StallInfo from "./stallInfo";
+import { supabase } from "./supabaseData";
+import "./hawkerMainScreen.css";
 
 export const HawkerMain = () => {
-  const [ name, setName ] = useState("");
-  const [ location, setLocation ] = useState("");
-  const [ price, setPrice ] = useState(0);
-  const [ taste, setTaste ] = useState(0);
-  const [ comment, setComment ] = useState("");
-  const [ stalls, setStalls ] = useState(null);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState(0);
+  const [taste, setTaste] = useState(0);
+  const [comment, setComment] = useState("");
+  const [stalls, setStalls] = useState(null);
   const [error, setError] = useState(null);
 
   const getStalls = useCallback(() => {
@@ -26,22 +35,22 @@ export const HawkerMain = () => {
       .catch((error) => {
         setError(error);
       });
-  }, [setStalls, setError])
+  }, [setStalls, setError]);
 
   useEffect(() => {
     getStalls();
-  }, [getStalls])
+  }, [getStalls]);
 
   const addStall = (event) => {
     supabase
       .from("stalls")
-      .insert({ 
+      .insert({
         name: name,
         location: location,
         price: price,
         taste: taste,
-        comment: comment
-       })
+        comment: comment,
+      })
       .then(({ error }) => {
         if (error) {
           setError(error);
@@ -53,64 +62,68 @@ export const HawkerMain = () => {
   };
 
   return (
-    <div>
-        <ResponsiveAppBar />
-        <Navbar>
-          <Container>
-            <Navbar.Brand>Stalls</Navbar.Brand>
-            <Nav>
-              <Nav.Item>in NUS-Bite</Nav.Item>
-            </Nav>
-          </Container>
-        </Navbar>
+    <div className="hMS">
+      <ResponsiveAppBar />
+      <Navbar>
         <Container>
-          <Row>
-            <h3>Add Your Stall</h3>
-            <Form.Label>Stall Name</Form.Label>
-            <Form.Control 
-              type="text" 
-              id="name"
-              onChange={(e) => setName(e.target.value)}
-              />
-            <br></br>
-            <Form.Label>Stall Location</Form.Label>
-            <Form.Control 
-              type="text" 
-              id="location"
-              onChange={(e) => setLocation(e.target.value)}
-              />
-            <br></br>
-            <Form.Label>Price</Form.Label>
-            <Form.Control 
-              type="number" 
-              id="price"
-              onChange={(e) => setPrice(e.target.value)}
-              />
-            <br></br>
-            <Form.Label>Taste</Form.Label>
-            <Form.Control 
-              type="number" 
-              id="taste"
-              onChange={(e) => setTaste(e.target.value)}
-              />
-            <br></br>
-            <Form.Label>Comment</Form.Label>
-            <Form.Control 
-              type="text" 
-              id="comment"
-              onChange={(e) => setComment(e.target.value)}
-              />
-            <br></br>
-            <button onClick={() => addStall()}>Submit</button>
-          </Row>
-          <hr></hr>
-          <h3>Stalls Added</h3>
-          <Row>
-            {stalls && stalls.map((stalls) => (
-              <StallInfo stalls={stalls}/>
-            ))}
-          </Row>
+          <Navbar.Brand>Stalls</Navbar.Brand>
+          <Nav>
+            <Nav.Item>in NUS-Bite</Nav.Item>
+          </Nav>
         </Container>
+      </Navbar>
+      <Container className="container">
+        <Row>
+          <h3>Add Your Stall</h3>
+          <Form.Label>Stall Name </Form.Label>
+          <Form.Control
+            type="text"
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br></br>
+          <Form.Label>Stall Location </Form.Label>
+          <Form.Control
+            type="text"
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <br></br>
+          <Form.Label>Price </Form.Label>
+          <Form.Control
+            type="number"
+            id="price"
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <br></br>
+          <Form.Label>Taste </Form.Label>
+          <Form.Control
+            type="number"
+            id="taste"
+            onChange={(e) => setTaste(e.target.value)}
+          />
+          <br></br>
+          <Form.Label>Comment </Form.Label>
+          <Form.Control
+            type="text"
+            id="comment"
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <br></br>
+          <button onClick={() => addStall()}>Submit</button>
+        </Row>
+        <hr></hr>
+        <h3>Stalls Added</h3>
+        <Row>
+          {stalls &&
+            stalls.map((stalls) => (
+              <div className="list">
+                {" "}
+                <StallInfo stalls={stalls} />{" "}
+              </div>
+            ))}
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
--- my-app/src/component/stallInfo.js
+++ my-app/src/component/stallInfo.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Card, Button, Form } from 'react-bootstrap'
-import { useState } from 'react';
-import { supabase } from './supabaseData';
+import React from "react";
+import { Card, Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { supabase } from "./supabaseData";
+import "./hawkerMainScreen.css";
 
 function StallInfo(props) {
   const stalls = props.stalls;
 
-  const [ edit, setEdit ] = useState(false);
-  const [ name, setName ] = useState("");
-  const [ location, setLocation ] = useState("");
-  const [ price, setPrice ] = useState(0);
-  const [ taste, setTaste ] = useState(0);
-  const [ comment, setComment ] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [price, setPrice] = useState(0);
+  const [taste, setTaste] = useState(0);
+  const [comment, setComment] = useState("");
 
   const handleDelete = (id) => {
     supabase
@@ -28,79 +29,87 @@ function StallInfo(props) {
   const updateStall = (id) => {
     supabase
       .from("stalls")
-      .update({ 
+      .update({
         name: name,
         location: location,
         price: price,
         taste: taste,
-        comment: comment
-       })
+        comment: comment,
+      })
       .eq("id", id)
       .then(({ error }) => {
         if (error) {
-            console.log(error.message);
-            alert("Failed to update task!");
+          console.log(error.message);
+          alert("Failed to update task!");
         }
       });
   };
 
   return (
-    <Card style={{width: "18rem"}}>
+    <div className="stallsadded">
+      <Card style={{ textAlign: "center" }}>
         <Card.Body key="stalls.id">
-            { edit == false ?
-                <>
-                <Card.Title>{stalls.name}</Card.Title>
-                <Card.Text>{stalls.location}</Card.Text>
-                <Button variant="danger" onClick={() => handleDelete(stalls.id)}>Delete Stall</Button>
-                <Button variant="secondary" onClick={() => setEdit(true)}>Edit Stall Info</Button>
-                </>
-            : 
-                <>
-                    <h4>Edit Stall Info</h4>
-                    <Button size="sm" onClick={() => setEdit(false)}>Return</Button>
-                    <br></br>
-                    <Form.Label>Stall Name</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    id="name"
-                    defaultValue={stalls.name}
-                    onChange={(e) => setName(e.target.value)}
-                    />
-                    <br></br>
-                    <Form.Label>Stall Location</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    id="location"
-                    defaultValue={stalls.location}
-                    onChange={(e) => setLocation(e.target.value)}
-                    />
-                    <br></br>
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control 
-                    type="number" 
-                    id="price"
-                    onChange={(e) => setPrice(e.target.value)}
-                    />
-                    <br></br>
-                    <Form.Label>Taste</Form.Label>
-                    <Form.Control 
-                    type="number" 
-                    id="taste"
-                    onChange={(e) => setTaste(e.target.value)}
-                    />
-                    <br></br>
-                    <Form.Label>Comment</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    id="comment"
-                    onChange={(e) => setComment(e.target.value)}
-                    />
-                    <br></br>
-                    <button onClick={() => updateStall(stalls.id)}>Submit</button>
-                </>
-            }
+          {edit === false ? (
+            <>
+              <Card.Title>Stall Name: {stalls.name}</Card.Title>
+              <Card.Text>Location: {stalls.location}</Card.Text>
+              <Button variant="danger" onClick={() => handleDelete(stalls.id)}>
+                Delete Stall
+              </Button>
+              <Button variant="secondary" onClick={() => setEdit(true)}>
+                Edit Stall Info
+              </Button>
+            </>
+          ) : (
+            <>
+              <h4>Edit Stall Info</h4>
+              <Button size="sm" onClick={() => setEdit(false)}>
+                Return
+              </Button>
+              <br></br>
+              <Form.Label>Stall Name</Form.Label>
+              <Form.Control
+                type="text"
+                id="name"
+                defaultValue={stalls.name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <br></br>
+              <Form.Label>Stall Location</Form.Label>
+              <Form.Control
+                type="text"
+                id="location"
+                defaultValue={stalls.location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+              <br></br>
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                id="price"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <br></br>
+              <Form.Label>Taste</Form.Label>
+              <Form.Control
+                type="number"
+                id="taste"
+                onChange={(e) => setTaste(e.target.value)}
+              />
+              <br></br>
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                type="text"
+                id="comment"
+                onChange={(e) => setComment(e.target.value)}
+              />
+              <br></br>
+              <button onClick={() => updateStall(stalls.id)}>Submit</button>
+            </>
+          )}
         </Card.Body>
-    </Card>
-  )
+      </Card>
+    </div>
+  );
 }
 export default StallInfo;
